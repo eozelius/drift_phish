bde744a7a893030483583752a1494565d83747fc
ethan ozelius <e.ozelius@gmail.com>
2017-11-06 18:33:06 -0500

    test the chess#board inits with black and white pieces in the right places

--- spec/unit/chess_spec.rb
+++ spec/unit/chess_spec.rb
@@ -9,8 +9,41 @@ RSpec.describe Chess do
     let(:c) { Chess.new }
 
     it 'should create a board' do
-      expect(c.board[0][0].class).to eq(Piece)
-      expect(c.board[4][4]).to eq(0)
+      expect(c).to respond_to(:board)
+    end
+
+    it 'should have white pieces' do
+      rook  = c.board[7][0]
+      queen = c.board[7][3]
+      king  = c.board[7][4]
+
+      expect(rook.type).to  eq('rook')
+      expect(rook.color).to eq('white')
+
+      expect(queen.type).to  eq('queen')
+      expect(queen.color).to eq('white')
+
+      expect(king.type).to  eq('king')
+      expect(king.color).to eq('white')
+    end
+
+    it 'should have black pieces' do
+      knight = c.board[0][6]
+      queen  = c.board[0][3]
+      king   = c.board[0][4]
+      bishop = c.board[0][2]
+
+      expect(knight.type).to  eq('knight')
+      expect(knight.color).to eq('black')
+
+      expect(queen.type).to   eq('queen')
+      expect(queen.color).to  eq('black')
+
+      expect(king.type).to    eq('king')
+      expect(king.color).to   eq('black')
+
+      expect(bishop.type).to  eq('bishop')
+      expect(bishop.color).to eq('black')
     end
 
     # it 'should set the active_player to white'
